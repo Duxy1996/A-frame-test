4528ef41442ed4c052f90ab4d5e1f497e0d1d369
duxy1996 <durancdr96@gmail.com>
2017-11-30 20:32:47 +0100

    some things added

--- js/index_pc.js
+++ js/index_pc.js
@@ -1,7 +1,25 @@
 var xx = 0
 var sceneEl = document.querySelector('a-scene');
-camera = sceneEl.querySelector('#camera');
-camera.setAttribute('position', {x: 0, y: 3.75, z: 0});
+//camera = sceneEl.querySelector('#camera');
+//camera.setAttribute('position', {x: 0, y: 3.75, z: 0});
+
+console.log(AFRAME)
+AFRAME.registerPrimitive('a-ocean', {
+  // Attaches the `ocean` component by default.
+  // Defaults the ocean to be parallel to the ground.
+  defaultComponents: {
+    ocean: {},
+    rotation: {x: -90, y: 0, z: 0}
+  },
+  // Maps HTML attributes to the `ocean` component's properties.
+  mappings: {
+    width: 'ocean.width',
+    depth: 'ocean.depth',
+    density: 'ocean.density',
+    color: 'ocean.color',
+    opacity: 'ocean.opacity'
+  }
+});
 
 for (i = 0; i < 10; i++) { 
     var entityEl = document.createElement('a-cylinder');
@@ -25,11 +43,5 @@ for (i = 0; i < 10; i++) {
     sceneEl.appendChild(entityEl);
 }
 
-function WhichButton(event) {   
-    camera = sceneEl.querySelector('#camera');
-    console.log(camera.components.rotation.data.y)
-    zz = camera.components.position.attrValue.z - Math.cos(camera.components.rotation.data.y*3.1415/180)
-    xx = camera.components.position.attrValue.x - Math.sin(camera.components.rotation.data.y*3.1415/180)
-    camera.setAttribute('position', {x: xx, y: 3.75, z: zz});
-}
+
 

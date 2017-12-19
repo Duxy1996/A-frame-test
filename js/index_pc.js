3d1e2018578c88def77efcbf2a5f5e4683196433
duxy1996 <durancdr96@gmail.com>
2017-12-19 21:13:57 +0100

    added a little stairs and phisics for them, and one light

--- js/index_pc.js
+++ js/index_pc.js
@@ -1,5 +1,3 @@
-var time = 0
-
 var sceneEl = document.querySelector('a-scene');
 camera = sceneEl.querySelector('#camera');
 camera.setAttribute('position', {x: 0, y: 1.5, z: 5});
@@ -64,7 +62,85 @@ AFRAME.registerPrimitive('a-prism', extendDeep({}, meshMixin, {
   }
 }));
 
-function pilar(x,y,z) {
+function WhichButton(event) { 
+  camera = sceneEl.querySelector('#camera');
+  zz = camera.components.position.attrValue.z - Math.cos(camera.components.rotation.data.y*3.1415/180);
+  xx = camera.components.position.attrValue.x - Math.sin(camera.components.rotation.data.y*3.1415/180);
+  console.log(xx,zz)
+  if(((xx > -2.5)&&(xx<23.5))&&(zz < 1.95)){    
+    camera.setAttribute('position', {x: xx, y: 1.85, z: zz});
+    if(zz < (1.95 - 0.5)){
+      camera.setAttribute('position', {x: xx, y: 2.15, z: zz});
+    }
+    if(zz < (1.95 - 1)){
+      camera.setAttribute('position', {x: xx, y: 2.5, z: zz});
+    }
+  }
+  else{
+    camera.setAttribute('position', {x: xx, y: 1.5, z: zz});
+  }
+}
+
+setInterval(function(){
+  light = sceneEl.querySelector('#light');
+  xx = light.components.position.attrValue.x + 0.003;
+  light.setAttribute('position', {x: xx, y: 7, z: 9});
+}, 30);
+
+function ground(x,y,z,e,w,h) {
+  var entityEl = document.createElement('a-plane');
+  entityEl.setAttribute('geometry', {
+    width: w,
+    height: h
+  });  
+  entityEl.setAttribute('rotation', {x: e, y: 0, z: 0});
+  entityEl.setAttribute('position', {x: x, y: y, z: z});
+  entityEl.setAttribute('material', 'color', '#8C6B21');
+  entityEl.setAttribute('shadow','receive','true');
+  sceneEl.appendChild(entityEl);
+}
+
+function friso(x,y,z){
+  var friso = document.createElement('a-triangle');
+  friso.setAttribute('geometry', {  
+    vertexA: {x: 0, y: 1.5, z: 0},
+    vertexB: {x: -9, y: -1, z: 0},
+    vertexC: {x: 9, y: -1, z: 0}
+  });
+
+  friso.setAttribute('rotation', {x: 0, y: 0, z: 0});
+  friso.setAttribute('position', {x: 10.75 + x, y: 9.7 + y, z: 1.1 + z});
+  friso.setAttribute('src', '#platform');
+  sceneEl.appendChild(friso);
+}
+
+function roof(x,y,z){
+  var roof = document.createElement('a-prism');
+  roof.setAttribute('geometry', {  
+    height: 0.25,
+    width: 9,
+    depth: 50
+  });
+  roof.setAttribute('rotation', {x: 0, y: 0, z: 225});
+  roof.setAttribute('position', {x: 10.75 + x, y: 8 + y, z: -24 + z});
+  roof.setAttribute('material', 'color', '#FFC65D');
+  roof.setAttribute('shadow','cast','true');
+  roof.setAttribute('shadow','receive','true');
+  sceneEl.appendChild(roof);
+
+  roof = document.createElement('a-prism');
+  roof.setAttribute('geometry', {  
+    height: 0.3,
+    width: 9.5,
+    depth: 2
+  });
+  roof.setAttribute('rotation', {x: 0, y: 0, z: 225});
+  roof.setAttribute('position', {x: 10.75 + x, y: 8.01 + y, z: +0.01 + z});
+  roof.setAttribute('material', 'color', '#FFC65D');
+  sceneEl.appendChild(roof);
+}
+
+function column(x,y,z) {
   var column = document.createElement('a-cylinder');
   column.setAttribute('geometry', {
     height: 7.5,
@@ -107,82 +183,65 @@ function pilar(x,y,z) {
   box.setAttribute('material', 'color', '#C6B566');
   box.setAttribute('shadow','cast','true');
   sceneEl.appendChild(box);
-
 }
 
-for (i = 0; i < 17; i++) { 
-  pilar(0,0,-i*3);    
+function columns(x,y,z){
+  for (i = 0 - z/3; i < 17 - z/3; i++) { 
+    column(x,y,-i*3);    
+  }
+
+  for (i = 0 - z/3; i < 17 - z/3; i++) { 
+    column(21.5+x,y,-i*3); 
+  }
+
+  for (i = 0 + x/3; i < 6 + x/3; i++) { 
+    column(3*i+3,y,z);
+  }
 }
 
-for (i = 0; i < 17; i++) { 
-  pilar(21.5,0,-i*3); 
+function partenon(x,y,z){
+  friso(x,y+1,z);
+  roof(x,y+1,z);
+  columns(x,y+1,z);
+  floor_part(x,y,z)
 }
 
-for (i = 0; i < 6; i++) { 
-  pilar(3*i+3,0,0);
+function floor_part(x,y,z){
+  var box = document.createElement('a-box');
+  box.setAttribute('geometry', {
+    height:0.34,
+    width: 26,
+    depth: 52
+  });
+  box.setAttribute('position', {x: x + 11, y: 0.25 + y, z: z - 24});
+  box.setAttribute('material', 'color', '#C6B566');
+  box.setAttribute('shadow','cast','true');
+  sceneEl.appendChild(box);
+
+  var box = document.createElement('a-box');
+  box.setAttribute('geometry', {
+    height:0.33,
+    width: 25,
+    depth: 51
+  });
+  box.setAttribute('position', {x: x + 11, y: 0.63 + y, z: z - 24});
+  box.setAttribute('material', 'color', '#C6B566');
+  box.setAttribute('shadow','cast','true');
+  sceneEl.appendChild(box);
+
+  var box = document.createElement('a-box');
+  box.setAttribute('geometry', {
+    height:0.33,
+    width: 24,
+    depth: 50
+  });
+  box.setAttribute('position', {x: x + 11, y: 0.96 + y, z: z - 24});
+  box.setAttribute('material', 'color', '#C6B566');
+  box.setAttribute('shadow','cast','true');
+  sceneEl.appendChild(box);
 }
 
-var roof = document.createElement('a-prism');
-roof.setAttribute('geometry', {  
-  height: 0.25,
-  width: 9,
-  depth: 50
-});
-roof.setAttribute('rotation', {x: 0, y: 0, z: 225});
-roof.setAttribute('position', {x: 10.75, y: 8, z: -24});
-roof.setAttribute('material', 'color', '#FFC65D');
-roof.setAttribute('shadow','cast','true');
-roof.setAttribute('shadow','receive','true');
-sceneEl.appendChild(roof);
-
-roof = document.createElement('a-prism');
-roof.setAttribute('geometry', {  
-  height: 0.3,
-  width: 9.5,
-  depth: 2
-});
-roof.setAttribute('rotation', {x: 0, y: 0, z: 225});
-roof.setAttribute('position', {x: 10.75, y: 8.01, z: +0.01});
-roof.setAttribute('material', 'color', '#FFC65D');
-sceneEl.appendChild(roof);
-
-ground(10,0,-30,-90)
-
-function ground(x,y,z,e) {
-  var entityEl = document.createElement('a-plane');
-  entityEl.setAttribute('geometry', {
-    width: 40,
-    height: 70
-  });  
-  entityEl.setAttribute('rotation', {x: e, y: 0, z: 0});
-  entityEl.setAttribute('position', {x: x, y: y, z: z});
-  entityEl.setAttribute('material', 'color', '#8C6B21');
-  entityEl.setAttribute('shadow','receive','true');
-  sceneEl.appendChild(entityEl);
-}
-
-function WhichButton(event) { 
-  camera = sceneEl.querySelector('#camera');
-  zz = camera.components.position.attrValue.z - Math.cos(camera.components.rotation.data.y*3.1415/180);
-  xx = camera.components.position.attrValue.x - Math.sin(camera.components.rotation.data.y*3.1415/180);
-  camera.setAttribute('position', {x: xx, y: 1.5, z: zz});
-}
-
-
-friso = document.createElement('a-triangle');
-friso.setAttribute('geometry', {  
-  vertexA: {x: 0, y: 1.5, z: 0},
-  vertexB: {x: -9, y: -1, z: 0},
-  vertexC: {x: 9, y: -1, z: 0}
-});
-
-friso.setAttribute('rotation', {x: 0, y: 0, z: 0});
-friso.setAttribute('position', {x: 10.75, y: 9.7, z: 1.1});
-friso.setAttribute('src', '#platform');
-sceneEl.appendChild(friso);
-
-setInterval(function(){
-  light = sceneEl.querySelector('#light');
-  xx = light.components.position.attrValue.x + 0.003;
-  light.setAttribute('position', {x: xx, y: 7, z: 9});
-}, 30);
+partenon(0,0,0);
+ground(10,0.1,7,-90,40,10);
+ground(-2.5,0.1,-3,-90,15,10);
+ground(+0.5,0.1,-13-16,-90,15,42);
